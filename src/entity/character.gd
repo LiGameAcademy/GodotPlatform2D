17bb186b88a0eb7ff4afddfc22fa08f1160792cd
jeremyLee0512 <2410250284@qq.com>
2024-02-16 23:00:26 +0800

    动画状态机 二段跳

--- src/entity/character.gd
+++ src/entity/character.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var state_machine : AnimationNodeStateMachinePlayback = animation_tree.get("parameters/StateMachine/playback")
 
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
@@ -7,15 +10,28 @@ extends CharacterBody2D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var can_double_jump = true
+
+func _process(delta: float) -> void:
+	if velocity.x != 0:
+		sprite_2d.flip_h = velocity.x < 0
 
 func _physics_process(delta: float) -> void:
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	else:
+		can_double_jump = true
+		
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		elif can_double_jump: # 如果还可以进行二段跳
+			velocity.y = JUMP_VELOCITY
+			can_double_jump = false # 使用掉二段跳机会
+			state_machine.travel("double_jump") # 切换到二段跳动画
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
--- src/scenes/level.gd
+++ src/scenes/level.gd
@@ -1,8 +1,16 @@
 extends Node2D
 
 @onready var parallax_background: ParallaxBackground = $ParallaxBackground
-@export var speed : float = 80
 @onready var parallax_layer: ParallaxLayer = $ParallaxBackground/ParallaxLayer
+@onready var control: Control = $CanvasLayer/Control
+@onready var mask_dude: CharacterBody2D = $mask_dude
+
+@export var speed : float = 80
+
+func _ready() -> void:
+	var material = control.material
+	if material is ShaderMaterial:
+		material.set_shader_parameter("character_position", Vector3(mask_dude.position.x, mask_dude.position.y, 0))
 
 func _process(delta: float) -> void:
 	## 在每一帧改变视差层的motion_offset属性的x值即可实现视差背景的滚动效果
